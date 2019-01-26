b654c792de867a62445a593dc8db33ada00067cf
Brendan K <brendanmkelly94@gmail.com>
2019-01-25 23:22:47 -0600

    added function that compares the two player choices and adds wins or losses to the respective player

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -14,9 +14,14 @@ database = firebase.database();
 var userNames;
 var clientUserName;
 var slot1;
+var slot2;
 var clientSlot;
 var clientChoice;
 var clientPlaying;
+var player1Choice;
+var player2Choice;
+var player1Score= {wins: 0, losses: 0, ties: 0};
+var player2Score= {wins: 0, losses: 0, ties: 0}
 
 //function that will grab usernames from database
 database.ref().on("value", function (snapshot) {
@@ -29,7 +34,22 @@ database.ref().on("value", function (snapshot) {
 database.ref("/player-1").on("value", function (snapshot) {
     //grabs the status of slot 1
     slot1 = snapshot.val().active;
+    console.log(player1Choice);
     console.log(slot1);
+    if (snapshot.child("choice").exists()){
+        player1Choice = snapshot.val().choice;
+        console.log(player1Choice);
+    }
+})
+database.ref("/player-2").on("value", function (snapshot) {
+    //grabs the status of slot 1
+    slot2 = snapshot.val().active;
+    console.log(player2Choice);
+    console.log(slot2);
+    if (snapshot.child("choice").exists()){
+        player2Choice = snapshot.val().choice;
+        console.log(player2Choice);
+    }
 })
 
 //function that allows user to choose a user name
@@ -99,6 +119,37 @@ var clientSelection = function (choice) {
 //function that locks in client choice and sends it to database based on the client's slot
 var sumbitChoice = function () {
     database.ref("/player-" + clientSlot + "/choice").set(clientChoice);
+}
+
+//function that compares the choices of the two players
+var compare = function () {
+    //if both players have submitted a choice
+    if(player1Choice != undefined && player2Choice != undefined){
+        console.log("making a comparison");
+        //the users tied
+        if(player1Choice === player2Choice){
+            player1Score.ties++;
+            database.ref("/player-1/ties").set(player1Score.ties);
+            player2Score.ties++;
+            database.ref("/player-2/ties").set(player2Score.ties);
+        }
+        //player1 wins
+        else if((player1Choice === "r" && player2Choice === "s")||
+        (player1Choice === "p" && player2Choice === "r")||
+        (player1Choice === "s" && player2Choice === "p")){
+            player1Score.wins++;
+            database.ref("/player-1/wins").set(player1Score.wins);
+            player2Score.losses++;
+            database.ref("/player-2/losses").set(player2Score.losses);
+        }
+        //player2 wins
+        else{
+            player1Score.losses++;
+            database.ref("/player-1/losses").set(player1Score.losses);
+            player2Score.wins++;
+            database.ref("/player-2/wins").set(player2Score.wins);
+        }
+    }
 }
 
 $(document).on("click","#submit-user-name",function(event){

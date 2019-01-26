c9188e6e007710e4a75743d30b652dfc3547fd45
Brendan K <brendanmkelly94@gmail.com>
2019-01-25 19:41:39 -0600

    initialized firebase and added psuedo code for player-1

---
+++ assets/javascript/logic.js
@@ -0,0 +1,21 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyA1ZgGfLQmdbqDJ-ow7mLxZP0pem0XHOJk",
+    authDomain: "brendan-rps-multi.firebaseapp.com",
+    databaseURL: "https://brendan-rps-multi.firebaseio.com",
+    projectId: "brendan-rps-multi",
+    storageBucket: "brendan-rps-multi.appspot.com",
+    messagingSenderId: "778849431771"
+  };
+  firebase.initializeApp(config);
+
+  database = firebase.database();
+
+  //function that allows user to choose a user name
+
+  //function that allows user to take player-1 slot
+
+  //function that allows player-1 to select a choice for the game
+
+  //function that locks in player-1 choice
+
